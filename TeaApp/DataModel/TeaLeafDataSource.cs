349809f2651ea62453bf3fe5bec6f4a4c3b837f5
Allen <yufan_1990@163.com>
2014-04-19 18:03:31 +0800

    add new datasource class

---
+++ TeaApp/DataModel/TeaLeafDataSource.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace TeaApp.DataModel
+{
+    public class TeaLeafDataSource
+    {
+        private static List<TeaLeafItem> items = new List<TeaLeafItem>();
+
+        public async Task<List<TeaLeafItem>> GetItems()
+        {
+            if(items.Count == 0)
+            {
+                await LoadItemsAsync();
+            }
+
+            return items;
+        }
+
+        private async Task LoadItemsAsync()
+        {
+            JsonArray array = await JsonArrayReader.ReadArrayFromFile("TalkTea/tealeafs.json");
+
+            TeaLeafItem item = null;
+            JsonObject obj = null;
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                obj = array[i].GetObject();
+                if(obj != null)
+                {
+                    item = new TeaLeafItem();
+                    item.Name = obj["name"].GetString();
+                    item.Address = obj["content"].GetString();
+
+                    items.Add(item);
+                }
+            }
+        }
+    }
+
+    public class TeaLeafItem
+    {
+        public String Name { get; set; }
+
+        public String Address { get; set; }
+    }
+}
---
+++ TeaApp/DataModel/TeaPlaceDataSource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace TeaApp.DataModel
+{
+    public class TeaPlaceDataSource
+    {
+        private static Dictionary<String, String> Contents = new Dictionary<string, string>();
+
+        public async Task<String> GetProvinceContent(String province)
+        {
+            if (Contents.Count == 0)
+            {
+                await GetProvinces();
+            }
+            
+            return Contents[province];
+        }
+
+        private async Task GetProvinces()
+        {
+            JsonArray jsonArray = await JsonArrayReader.ReadArrayFromFile("TalkTea/provinces.json");
+
+            String name, content;
+            JsonObject obj;
+
+            for (int i = 0; i < jsonArray.Count; i++)
+            {
+                obj = jsonArray[i].GetObject();
+
+                if(obj != null)
+                {
+                    name = obj["name"].GetString().Substring(0,2);
+                    content = obj["content"].GetString();
+
+                    Contents.Add(name, content);
+                }
+            }
+        }
+    }
+}
